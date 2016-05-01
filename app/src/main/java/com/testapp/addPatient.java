3ee8e4d6d5342beafc7e1e88666084651dd4af7b
Matthias Schmidbauer <Matthias Schmidbauer>
2016-05-01 22:28:02 +0200

    Initial commit

---
+++ app/src/main/java/com/testapp/addPatient.java
@@ -0,0 +1,21 @@
+package com.testapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class addPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_patient);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
