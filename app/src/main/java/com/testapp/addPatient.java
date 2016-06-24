bf393523fbd7ce816980fcdceb66d5dbd3726765
Matthias <m_schmidbauer@web.de>
2016-06-24 14:19:00 +0200

    overview....

--- app/src/main/java/com/testapp/addPatient.java
+++ app/src/main/java/com/testapp/addPatient.java
@@ -1,13 +1,17 @@
 package com.testapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class addPatient extends AppCompatActivity {
+
+    private static Button patientAufnehmen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,14 @@ public class addPatient extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button addPatient = (Button) findViewById(R.id.btnPatientAufnehmen);
+        addPatient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.testapp.Overview");
+                startActivity(intent);
+            }
+        });
     }
 
 }
